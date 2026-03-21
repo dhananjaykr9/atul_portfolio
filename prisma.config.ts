c634c9beab967dc4524e70fccee7e99eefabd9b1
Dhananjay G Kharkar <dkharkar00@gmail.com>
2026-03-21 13:24:08 +0530

    change

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,11 @@
+import 'dotenv/config'
 import { defineConfig } from '@prisma/config'
 
 export default defineConfig({
+  migrations: {
+    seed: 'node prisma/seed.js',
+  },
   datasource: {
-    url: process.env.DATABASE_URL
-  }
+    url: process.env.DATABASE_URL!,
+  },
 })
