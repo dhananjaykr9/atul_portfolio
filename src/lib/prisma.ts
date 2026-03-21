d9bfca35b653303d0336c8754f839dd32f94e328
Dhananjay G Kharkar <dkharkar00@gmail.com>
2026-03-21 12:07:12 +0530

    fix: simplify prisma initialization to resolve vercel p1001 connection error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,7 @@
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
-const connectionString = `${process.env.DATABASE_URL}`
-
 const prismaClientSingleton = () => {
-  const pool = new Pool({ connectionString })
-  const adapter = new PrismaPg(pool as any)
-  return new PrismaClient({ adapter })
+  return new PrismaClient()
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
